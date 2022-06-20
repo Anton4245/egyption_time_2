f25be252c15f924fcda87723e643526c2189b7af
Anton4245 <anton.salamatov@yahoo.com>
2022-06-20 22:07:03 +0500

    forth

--- lib/features/modify_meeting/field_edit_form.dart
+++ lib/features/modify_meeting/field_edit_form.dart
@@ -6,6 +6,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
 
+//for Git
 class FieldEditForm extends StatelessWidget {
   const FieldEditForm({Key? key}) : super(key: key);
 
